5db43fad3b76ea8fa517ede0e3217101188a2820
Julia <diniullova2011@yandex.ru>
2021-08-30 11:04:53 +0400

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const brackets = {
+    [')']: '(',
+    ['}']: '{',
+    ['|']: '|',
+};
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+      let currentI = str[i];
+
+      if(bracketsConfig.includes(currentI)) {
+          stack.push(currentI);
+      }
+      else {
+          if (stack.length === 0) {
+              return false;
+          }
+
+          let topElement = stack[stack.length - 1];
+
+          if (bracketsConfig[currentI] === topElement) {
+              stack.pop();
+          }
+          else {
+              return false;
+          }
+      }
+  }
+  return stack.length === 0;
 }
